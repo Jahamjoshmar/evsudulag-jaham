65475763c5a3c9c4186bf176faf3fe08bcd6fd51
joshmar <joshmarjaham32@gmail.com>
2025-02-23 12:09:29 +0800

    new updates

---
+++ system/src/app/main/users/mock/UserData.ts
@@ -0,0 +1,59 @@
+import { UserModel } from "../model/UserModel";
+
+export const UserData: UserModel[] = [
+    {
+        name: 'kyrie Apollo',
+        bday: 'Oct 18 2005',
+        age: 19,
+        address: 'Mayorga, Leyte',
+        hobbies: 'biking',
+        username: 'kyrie apollo@gmail.com',
+        password: '2456',
+        id: 1,
+        imgUrl: 'kyrie.jpg'
+    },
+    {
+        name: 'Jemarie Hughes',
+        bday: 'June 26 2005',
+        age: 19,
+        address: 'Mayorga, Leyte',
+        hobbies: 'dancing',
+        username: 'jemariehughes@gmail.com',
+        password: '1456',
+        id: 2,
+        imgUrl: 'jemarie.jpg'
+    },
+    {
+        name: 'Jack Prat',
+        bday: 'Dec 3 2005',
+        age: 19,
+        address: 'Mayorga, Leyte',
+        hobbies: 'singing',
+        username: 'jackprat@gmail.com',
+        password: '5678',
+        id: 3,
+        imgUrl: 'jack.jpg'
+    },
+    {
+        name: 'Justin bieber',
+        bday: 'Jan 2 2005',
+        age: 19,
+        address: 'Mayorga, Leyte',
+        hobbies: 'dancing',
+        username: 'justinbieber2@gmail.com',
+        password: '1234',
+        id: 4,
+        imgUrl: 'justin.jpg'
+    },
+    {
+        name: 'JOSHMAR JAHAM',
+        bday: 'SEPTEMBER 28 2005',
+        age: 19,
+        address: 'Mayorga, Leyte',
+        hobbies: 'dancing',
+        username: 'joshmarjaham32@gmail.com',
+        password: 'MDXEST_15',
+        id: 5,
+        imgUrl: 'joshmar.jpg'
+    },
+]
---
+++ system/src/app/main/users/model/UserModel.ts
@@ -0,0 +1,11 @@
+export interface UserModel {
+    name: string;
+    bday: string;
+    age: number;
+    address: string;
+    hobbies: string;
+    username: string;
+    password: string;
+    id: number;
+    imgUrl: string;
+}
