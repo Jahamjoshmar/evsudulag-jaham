b9f3cad61d4a2523509fe84210aa002d745b7d5d
joshmar <joshmarjaham32@gmail.com>
2025-03-10 19:56:21 +0800

    activity-4 files

--- lumen/app/Http/Controllers/UserController.php
+++ lumen/app/Http/Controllers/UserController.php
@@ -3,15 +3,67 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
-    public function getUsers()
+    public function login(Request $request)
     {
-        $users = [
-            ['id' => 1, 'name' => 'Joshmar', 'email' => 'josh@example.com'],
-            ['id' => 2, 'name' => 'Jane Smith', 'email' => 'jane@example.com']
-        ];
-        return response()->json($users);
+        // Validate input
+        $this->validate($request, [
+            'username' => 'required|string',
+            'password' => 'required|string'
+        ]);
+
+        // Find user by username
+        $user = DB::table('users') ->where('username', $request->username)->first();
+
+        // If no user found
+        if (!$user) {
+            return response()->json([
+                'message' => 'no user found'
+            ], 404);
+        } else {
+             // if password is invalid
+            if($request->password !== $user->password){
+                return response()->json([
+                    'message' => 'incorrect password'
+                ], 401);
+            }
+        }
+
+        return response()->json([
+            'message' => 'Login Successful',
+            'user' => [
+                'id' => $user->id,
+                'username' => $user->username,
+                'name' => $user->name,
+                'age' => $user->age
+            ]
+            ]);
+        
     }
 }
+
+
+
+// .env
+// DB_DATABASE = pos
+// create a database with the name pos
+// run this command
+// php artisan migrate
+// add record to the database
+// php -S localhost:8000 -t public
+// postman
+// body -> username and passoword
+
+
+
+
+
+
+
+
+
+
+
--- lumen/routes/web.php
+++ lumen/routes/web.php
@@ -18,3 +18,6 @@ $router->get('/', function () use ($router) {
 });
 
 $router->get('/users', 'UserController@getUsers');
+
+
+$router->post('/login', 'UserController@login');
